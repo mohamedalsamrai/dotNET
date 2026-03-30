86c2176e4899b0862c45acdfbcc2ff4b15e6a146
Mohammed Al-Samarraie <mohamed.admed@coda.com.iq>
2026-03-30 14:54:23 +0300

    init

---
+++ day-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace day_1;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        String? input = Console.ReadLine();
+        if (input != null&&!input.Equals(string.Empty))
+        {
+            string[] inputArray = input.Split(",");
+        }
+        else
+        {
+            Console.WriteLine("Nothing found");
+        }
+    }
+}
